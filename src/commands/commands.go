007ddce7717c817724d1332bf14ecfbca3bd98a1
zzzang12 <wadevs12@gmail.com>
2026-04-19 15:34:32 +0900

    Refactor: 반복되는 "Main" 파일명을 FILE_NAME 상수로 추출
    - src/commands/commands.go에 FILE_NAME 상수 정의 및 적용
    - src/services/problem.go에서 해당 상수를 사용하도록 리팩토링 및 . 임포트 적용

--- src/commands/commands.go
+++ src/commands/commands.go
@@ -1,5 +1,7 @@
 package commands
 
+const FILE_NAME = "Main"
+
 type Command struct {
 	BuildCmd  []string
 	RunCmd    []string
@@ -8,43 +10,43 @@ type Command struct {
 
 var Commands = map[string]Command{
 	"C": {
-		BuildCmd:  []string{"gcc", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.c", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/Main", "-O2", "-Wall", "-lm", "-static", "-std=gnu99"},
-		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
-		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
+		BuildCmd:  []string{"gcc", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".c", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME, "-O2", "-Wall", "-lm", "-static", "-std=gnu99"},
+		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
+		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
 	},
 	"CPP": {
-		BuildCmd:  []string{"g++", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.cpp", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/Main", "-O2", "-Wall", "-lm", "-static", "-std=gnu++17"},
-		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
-		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
+		BuildCmd:  []string{"g++", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".cpp", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME, "-O2", "-Wall", "-lm", "-static", "-std=gnu++17"},
+		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
+		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
 	},
 	"JAVA": {
-		BuildCmd:  []string{"javac", "-J-Xms1024m", "-J-Xmx1920m", "-J-Xss512m", "-encoding", "UTF-8", "-d", "bin", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.java"},
-		RunCmd:    []string{"java", "-Xms1024m", "-Xmx1920m", "-Xss512m", "-Dfile.encoding=UTF-8", "-XX:+UseSerialGC", "-cp", "bin", "Main"},
+		BuildCmd:  []string{"javac", "-J-Xms1024m", "-J-Xmx1920m", "-J-Xss512m", "-encoding", "UTF-8", "-d", "bin", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".java"},
+		RunCmd:    []string{"java", "-Xms1024m", "-Xmx1920m", "-Xss512m", "-Dfile.encoding=UTF-8", "-XX:+UseSerialGC", "-cp", "bin", FILE_NAME},
 		DeleteCmd: []string{"rm", "-r", "{JUDGE_TYPE}/{SUBMIT_ID}/bin"},
 	},
 	"PYTHON": {
 		BuildCmd:  []string{},
-		RunCmd:    []string{"python3", "-W", "ignore", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.py"},
+		RunCmd:    []string{"python3", "-W", "ignore", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".py"},
 		DeleteCmd: []string{},
 	},
 	"JAVASCRIPT": {
 		BuildCmd:  []string{},
-		RunCmd:    []string{"node", "--stack-size=65536", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.js"},
+		RunCmd:    []string{"node", "--stack-size=65536", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".js"},
 		DeleteCmd: []string{},
 	},
 	"GO": {
-		BuildCmd:  []string{"go", "build", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/Main", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.go"},
-		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
-		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
+		BuildCmd:  []string{"go", "build", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME, "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".go"},
+		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
+		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
 	},
 	"KOTLIN": {
-		BuildCmd:  []string{"kotlinc", "-J-Xms1024m", "-J-Xmx1920m", "-J-Xss512m", "-include-runtime", "-d", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.jar", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.kt"},
-		RunCmd:    []string{"java", "-Xms1024m", "-Xmx1920m", "-Xss512m", "-Dfile.encoding=UTF-8", "-XX:+UseSerialGC", "-jar", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.jar"},
-		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.jar"},
+		BuildCmd:  []string{"kotlinc", "-J-Xms1024m", "-J-Xmx1920m", "-J-Xss512m", "-include-runtime", "-d", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".jar", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".kt"},
+		RunCmd:    []string{"java", "-Xms1024m", "-Xmx1920m", "-Xss512m", "-Dfile.encoding=UTF-8", "-XX:+UseSerialGC", "-jar", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".jar"},
+		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".jar"},
 	},
 	"SWIFT": {
-		BuildCmd:  []string{"swiftc", "-O", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/Main", "{JUDGE_TYPE}/{SUBMIT_ID}/Main.swift"},
-		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
-		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/Main"},
+		BuildCmd:  []string{"swiftc", "-O", "-o", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME, "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME + ".swift"},
+		RunCmd:    []string{"{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
+		DeleteCmd: []string{"rm", "{JUDGE_TYPE}/{SUBMIT_ID}/" + FILE_NAME},
 	},
 }
